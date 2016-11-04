f09caa1035c1e1d00a263a3d9614ae00edb5777c
Francesco Levanti <francesco@levanti.net>
2016-11-04 15:25:30 +0000

    mall change in bashrc and now the container CMD is basicly the apache2 call with foreground option. “Mind tricks don’t work on me.” – Watto

--- start_files/ubuntu/start_services.sh
+++ start_files/ubuntu/start_services.sh
@@ -1,9 +1,9 @@
-
-apache2ctl start
-
+# you can put here commands you want to execute when container is started.
 
 
 
 
 
-tail -f /dev/null
+#this needs to be the last command... :)
+apache2ctl -D FOREGROUND
+
